c6d605f3007d0bf3949624ad6ccafff7961f39a3
vincent <vincentc658@gmail.com>
2020-10-08 11:10:58 +0700

    done : - cleaning code

--- LibCamera/src/main/java/com/library/libcamera/CameraActivity.java
+++ LibCamera/src/main/java/com/library/libcamera/CameraActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
@@ -15,10 +14,8 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.Camera;
 import androidx.camera.core.CameraInfoUnavailableException;
 import androidx.camera.core.CameraSelector;
-import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageCapture;
 import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.ImageProxy;
@@ -31,11 +28,7 @@ import androidx.lifecycle.LifecycleOwner;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
-import java.io.File;
 import java.nio.ByteBuffer;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -44,7 +37,7 @@ public class CameraActivity extends AppCompatActivity {
 
     private Executor executor = Executors.newSingleThreadExecutor();
     private int REQUEST_CODE_PERMISSIONS = 1001;
-    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE"};
+    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA"};
     private ProcessCameraProvider cameraProvider;
     private int lensFacing= CameraSelector.LENS_FACING_BACK;
 
@@ -60,7 +53,7 @@ public class CameraActivity extends AppCompatActivity {
         captureImage = findViewById(R.id.rlButtonCapture);
 
         if (allPermissionsGranted()) {
-            startCamera(); //start camera if permission has been granted by user
+            startCamera();
         } else {
             ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
         }
@@ -70,32 +63,30 @@ public class CameraActivity extends AppCompatActivity {
 
         final ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
 
-        cameraProviderFuture.addListener(new Runnable() {
-            @Override
-            public void run() {
-                try {
+        cameraProviderFuture.addListener(() -> {
+            try {
 
-                    cameraProvider = cameraProviderFuture.get();
-                    int cameraFacing = getIntent().getIntExtra("isFrontCamera", 0);
-                    String instruction = getIntent().getStringExtra("instruction");
-                    TextView tvInstruction= findViewById(R.id.tvInstruction);
-                    tvInstruction.setText(instruction);
-                    ImageView bgOverlay = findViewById(R.id.bgOverlay);
-                    if(cameraFacing==0 && hasBackCamera()){
-                        lensFacing = CameraSelector.LENS_FACING_BACK;
-                        bgOverlay.setBackgroundResource(R.drawable.overlay_identity_card);
-                    }else if(cameraFacing==1 && hasFrontCamera()){
-                        lensFacing= CameraSelector.LENS_FACING_FRONT;
-                        bgOverlay.setBackgroundResource(R.drawable.overlay_selfie_with_id);
-                    }else{
-                        throw new IllegalStateException("Back and front camera are unavailable");
-                    }
-                    bindPreview(cameraProvider);
-
-                } catch (ExecutionException | InterruptedException e) {
-                    // No errors need to be handled for this Future.
-                    // This should never be reached.
+                cameraProvider = cameraProviderFuture.get();
+                int cameraFacing = getIntent().getIntExtra("isFrontCamera", 0);
+                String instruction = getIntent().getStringExtra("instruction");
+                TextView tvInstruction= findViewById(R.id.tvInstruction);
+                tvInstruction.setText(instruction);
+                ImageView bgOverlay = findViewById(R.id.bgOverlay);
+                if(cameraFacing==0 && hasBackCamera()){
+                    lensFacing = CameraSelector.LENS_FACING_BACK;
+                    bgOverlay.setBackgroundResource(R.drawable.overlay_identity_card);
+                }else if(cameraFacing==1 && hasFrontCamera()){
+                    lensFacing= CameraSelector.LENS_FACING_FRONT;
+                    bgOverlay.setBackgroundResource(R.drawable.overlay_selfie_with_id);
+                }else{
+                    throw new IllegalStateException("Back and front camera are unavailable");
                 }
+                bindPreview(cameraProvider);
+
+            } catch (ExecutionException | InterruptedException e) {
+                // No errors need to be handled for this Future.
+                // This should never be reached.
+                e.printStackTrace();
             }
         }, ContextCompat.getMainExecutor(this));
     }
@@ -109,26 +100,20 @@ public class CameraActivity extends AppCompatActivity {
                 .requireLensFacing(lensFacing)
                 .build();
 
-        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
-                .build();
 
         ImageCapture.Builder builder = new ImageCapture.Builder();
 
         final ImageCapture imageCapture = builder
-                .setTargetRotation(this.getWindowManager().getDefaultDisplay().getRotation())
+                .setTargetRotation(this.getDisplay().getRotation())
                 .build();
         cameraProvider.unbindAll();
         preview.setSurfaceProvider(mPreviewView.getSurfaceProvider());
 
-        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview, imageCapture);
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview, imageCapture);
 
         captureImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SimpleDateFormat mDateFormat = new SimpleDateFormat("yyyyMMddHHmmss", Locale.US);
-                File file = new File(getBatchDirectoryName(), mDateFormat.format(new Date()) + ".jpg");
-
-                ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions.Builder(file).build();
                 imageCapture.takePicture(executor, new ImageCapture.OnImageCapturedCallback() {
                     @Override
                     public void onCaptureSuccess(@NonNull ImageProxy image) {
@@ -150,18 +135,6 @@ public class CameraActivity extends AppCompatActivity {
                 });
             }
         });
-    }
-
-    public String getBatchDirectoryName() {
-
-        String app_folder_path = "";
-        app_folder_path = Environment.getExternalStorageDirectory().toString() + "/images";
-        File dir = new File(app_folder_path);
-        if (!dir.exists() && !dir.mkdirs()) {
-
-        }
-
-        return app_folder_path;
     }
 
     private boolean allPermissionsGranted() {
